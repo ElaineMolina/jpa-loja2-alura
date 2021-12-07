9315c3141ce27d62e739fc9ea1acdd41a970dd17
ElaineMolina <elainemolina10@gmail.com>
2021-12-07 11:01:23 -0300

    Mapeando novas entidades, class Cliente e class Pedido(relacionamento ManyToOne com Cliente

---
+++ src/main/java/br/com/molina/loja/modelo/Pedido.java
@@ -0,0 +1,51 @@
+package br.com.molina.loja.modelo;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal valorTotal;
+    private LocalDate data = LocalDate.now();
+
+
+    @ManyToOne
+    private Cliente cliente;
+
+    public Pedido() {
+    }
+
+    public Pedido(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
