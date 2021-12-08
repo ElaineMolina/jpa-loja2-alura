0997c4c4b7f56e08fe5f66f6b6774109db1bf101
ElaineMolina <elainemolina@gmail.com>
2021-12-08 12:05:25 -0300

    Consultas com select new

--- src/main/java/br/com/molina/loja/dao/PedidoDao.java
+++ src/main/java/br/com/molina/loja/dao/PedidoDao.java
@@ -2,6 +2,7 @@ package br.com.molina.loja.dao;
 
 import br.com.molina.loja.modelo.Pedido;
 import br.com.molina.loja.modelo.Produto;
+import br.com.molina.loja.vo.RelatorioDeVendasVo;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
@@ -20,4 +21,22 @@ public class PedidoDao {
         this.em.persist(pedido);
     }
 
+    public BigDecimal valorTotalVendido(){
+        String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
+        return em.createQuery(jpql, BigDecimal.class)
+                .getSingleResult();
+    }
+    public List<RelatorioDeVendasVo> relatorioDeVendas(){
+        String jpql = "SELECT new br.com.molina.loja.vo.RelatorioDeVendasVo("
+            + "produto.nome, "
+            + "SUM(item.quantidade), "
+            + "MAX(pedido.data)) "
+            + "FROM Pedido pedido "
+            + "JOIN pedido.itens item "
+            + "JOIN item.produto produto "
+            + "GROUP BY produto.nome "
+            + "ORDER BY item.quantidade DESC";
+        return em.createQuery(jpql, RelatorioDeVendasVo.class)
+            .getResultList();
+    }
 }
